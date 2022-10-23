addf60a388f934d8462af7ea09c46fea1b08c13a
Gabriel Antonelli <gabrielantonelli@hotmail.com>
2022-10-23 11:38:19 +0200

    generate contrast color text

--- src/App.js
+++ src/App.js
@@ -1,45 +1,60 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from './components/Card/Card';
 import availableCards from './components/Card/availableCards';
 import classes from './App.module.css';
 
 const App = () => {
-  const generateBackground = () => `#${Math.floor(Math.random()*16777215).toString(16)}`;
+  const generateTheme = () => {
+    const background = Math.floor(Math.random() * 16777215).toString(16).toUpperCase();
+    const color = (Number(`0x1${background}`) ^ 0xFFFFFF).toString(16).substr(1).toUpperCase();
+    return {
+      background: `#${background}`,
+      color: `#${color}`
+    }
+  } 
   const [lastKey, setLastKey] = useState('');
   const [activeKey, setActiveKey] = useState('');
-  const [background, setBackground] = useState('yellow');
+  const [theme, setTheme] = useState(generateTheme());
 
-  useEffect(()=>{
-    document.addEventListener('keydown',(e)=>{
+  useEffect(() => {
+    document.addEventListener('keydown', (e) => {
+      if (e.altKey) {
+        // disable also keys combination
+        if (e.keyCode == 67 && window.prevKey == 86)
+          e.preventDefault();
+        else if (e.keyCode == 86 && window.prevKey == 67)
+          e.preventDefault();
+        window.prevKey = e.keyCode
+      }
       e.preventDefault();
       setLastKey(e.key);
     })
-    document.addEventListener('click',(e)=>{
+    document.addEventListener('click', (e) => {
       e.preventDefault();
       const randomItemIndex = Math.floor(Math.random() * (availableCards.length));
       setLastKey(availableCards[randomItemIndex].key);
     });
 
-  },[]);
+  }, []);
 
 
-  useEffect(()=>{
-    const timer = setTimeout(()=>{
+  useEffect(() => {
+    const timer = setTimeout(() => {
       setActiveKey(lastKey);
-      setBackground(generateBackground());
-    },600);
+      setTheme(generateTheme());
+    }, 600);
     return () => clearTimeout(timer);
-  },[lastKey])
+  }, [lastKey])
 
-  
 
-  const activeItemIndex = availableCards.findIndex(item=> item.key === activeKey);
-  const activeItem = availableCards[activeItemIndex];
+
+  const activeItemIndex = availableCards.findIndex(item => item.key === activeKey);
+  const activeItem = availableCards[activeItemIndex] || availableCards[0];
 
   return (
-    <div className={classes.backdrop} style={{background: background}}>
-      <h1>Alberto Works!</h1>
-      {activeItem && <Card image={activeItem}/>}
+    <div className={classes.backdrop} style={{ background: theme.background }}>
+      <h1 style={{color: theme.color}}>Alberto Works!</h1>
+      {activeItem && <Card image={activeItem} />}
     </div>
   );
 };
